cb696b13ad8691fec7cdb14d158f3bb2f65da043
karl <karl.padu@voco.ee>
2026-01-09 11:35:25 +0200

    Komponendi sisustamine andmetega

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,26 @@ import './App.css'
 import ExpenseItem from './components/ExpenseItem'
 
 function App() {
+  const expenses = [
+    {
+      date: new Date(2026, 0, 9),
+      title: 'New book',
+      price: 30.99,
+    },
+    {
+      date: new Date(2026, 0, 9),
+      title: 'New book 2',
+      price: 799.49,
+    }
+  ];
 
   return (
       <div className="App">
-        <ExpenseItem />
+        <ExpenseItem data={expenses[0]}/>
+        <ExpenseItem data={expenses[1]}/>
 
       </div>
-  )
+  );
 }
 
 export default App
