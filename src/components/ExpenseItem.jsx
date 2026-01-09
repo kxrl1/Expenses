67165e14cff08668c175fe0a06d8cb159310f7bc
karl <karl.padu@voco.ee>
2026-01-09 11:44:32 +0200

    Komponendi jaotamine mitmeks komponendiks

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,13 +1,14 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate.jsx'
 
 const ExpenseItem = (props) => {
 
     console.log(props)
-    console.log(props.data)
+    console.log(props.data.date)
 
     return(
         <div className='expense-item'>
-            <div>{props.data.date.toString()}</div>
+            <ExpenseDate date={props.data.date} />
             <div className='expense-item__description'>
                 <h2>{props.data.title}</h2>
                 <div className='expense-item__price'>{props.data.price}</div>
