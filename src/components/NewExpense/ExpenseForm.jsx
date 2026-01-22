e59fcaf691aa2531507eb2da0ff72a07d7bd1b2b
karl <karl.padu@voco.ee>
2026-01-22 14:12:12 +0200

    Vormi andmete saatmise töötlus

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -31,20 +31,31 @@ const ExpenseForm = (props) => {
         });
     }
 
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: userInput.enteredTitle,
+            price: userInput.enteredPrice,
+            date: new Date(userInput.enteredDate)
+        };
+        console.log(expenseData);
+    }
+    setEnteredTitle('')
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input type="text" onChange={titleChangeHandler} value={userInput.enteredTitle} />
                 </div>
                 <div className="new-expense__control">
                     <label>Price</label>
-                    <input type="number" min="0.01" step="0.01" onChange={priceChangeHandler} />
+                    <input type="number" min="0.01" step="0.01" onChange={priceChangeHandler} value={userInput.enteredPrice} />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2026-01-01" max="2026-12-31" onChange={dateChangeHandler} />
+                    <input type="date" min="2026-01-01" max="2026-12-31" onChange={dateChangeHandler} value={userInput.enteredDate} />
                 </div>
             </div>
             <div className="new-expense__actions">
